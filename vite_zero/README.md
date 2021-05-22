f0afa038ade4daf0e26617eb52535606e07cb0e6
xiaofuyesnew <xiaofuyesnew@qq.com>
2021-05-23 00:18:47 +0800

    提交更新

--- vite_zero/README.md
+++ vite_zero/README.md
@@ -1,13 +1,23 @@
 # 从零开始搭建一个vite+vue3项目
 
 ```bash
+# 创建项目目录
 mkdir -p vite_zero
+
+# 进入该目录
+cd vite_zero
+
+# 初始化npm配置
+npm init -y
 ```
 
 ```bash
+# 安装项目依赖
 npm install vue@next vue-router@next vuex@next
 ```
 
 ```bash
+# 安装开发依赖
 npm install vite @vitejs/plugin-vue @vue/compiler-sfc --save-dev
 ```
+
---
+++ vite_zero/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <Hello name="Vite User" />
+</template>
+
+<style scoped>
+.wrap {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
+<script>
+import Hello from '../components/Hello.vue'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>

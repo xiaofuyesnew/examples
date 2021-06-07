94188fc72a1e76c42ae87b41d676e532baaf76f0
xiaofuyesnew <xiaofuyesnew@qq.com>
2021-06-07 18:21:40 +0800

    tijiao

--- vite_zero/src/main.js
+++ vite_zero/src/main.js
@@ -1,10 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
 
-const app = createApp(App)
-
-app.use(store).use(router)
-
-app.mount('#app')
+createApp(App).use(store).use(router).mount("#app");
